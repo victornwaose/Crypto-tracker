9fbc9f5a2a5c242ae0165d53af4532bcf6da1541
nwaose victor <nwaosevictor1234@gmail.com>
2021-10-16 02:07:09 +0100

    added Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header";
+import CoinPage from "./page/CoinPage";
 import HomePage from "./page/HomePage";
 
 function App() {
+    const useStyle = makeStyles(() => ({
+        App: {
+            backgroundColor: "#14161a",
+            color: "#fff",
+            minHeight: "100vh",
+        },
+    }));
+    const classes = useStyle();
+
     return (
         <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/coins/:id" exact component={HomePage} />
-            </Switch>
+            <div className={classes.App}>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/coins/:id" exact component={CoinPage} />
+                </Switch>
+            </div>
         </Router>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import {
+    AppBar,
+    Container,
+    createTheme,
+    makeStyles,
+    MenuItem,
+    Select,
+    Toolbar,
+    Typography,
+    ThemeProvider,
+} from "@material-ui/core";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>this is an header componentss</h1>
-        </div>
-    )
-}
+    const useStyle = makeStyles(() => ({
+        title: {
+            flex: 1,
+            color: "gold",
+            fontFamily: "Montserrat",
+            fontWeight: "bold",
+            cursor: "pointer",
+        },
+    }));
 
-export default Header
+    const classes = useStyle();
+
+    const history = useHistory();
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color="transparent" position="static">
+                <Container>
+                    <Toolbar>
+                        <Typography
+                            onClick={() => history.push("/")}
+                            className={classes.title}
+                            variant="h4"
+                        >
+                            Crypto-Tracker
+                        </Typography>
+                        <Select
+                            variant="outlined"
+                            style={{ width: 100, height: 40, marginRight: 15 }}
+                        >
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"Naira"}>Naira</MenuItem>
+                        </Select>
+                    </Toolbar>{" "}
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    );
+};
+
+export default Header;
