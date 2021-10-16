5482c67f0245208ac58726b6c532792cabca1f1c
nwaose victor <nwaosevictor1234@gmail.com>
2021-10-16 01:13:12 +0100

    crypto tracker

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>this is an header componentss</h1>
+        </div>
+    )
+}
+
+export default Header
