4ca54f9eab58af53b61c8dc861d684e564d82df6
nwaose victor <nwaosevictor1234@gmail.com>
2021-10-19 01:54:20 +0100

    added the single coin page

--- src/page/CoinPage.js
+++ src/page/CoinPage.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { SingleCoin } from "../api/api";
+
+import { CryptoState } from "../context/CryptoContext";
 
 const CoinPage = () => {
+    const { id } = useParams();
+    const [coin, setCoin] = useState([]);
+    const { currency, symbol } = CryptoState();
+
+    const fetchSingleCoins = async () => {
+        try {
+            const response = await fetch(SingleCoin(id));
+
+            if (response.ok) {
+                const data = await response.json();
+                setCoin(data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    console.log(coin);
+
+    useEffect(() => {
+        fetchSingleCoins();
+    }, []);
+
     return (
         <div>
             <h1>hello world</h1>
